e6a0409a09dafa28962cc271db072fe6d001238b
welaskez <nazar.nikitenko.03@mail.ru>
2024-07-20 01:20:39 +0500

    refactor is_admin filter

--- bot/config.py
+++ bot/config.py
@@ -6,11 +6,15 @@ import os
 
 load_dotenv()
 
+BASE_DIR = Path(__file__).resolve().parent
+
+ADMINS = os.getenv("ADMINS").split(" ")
+
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 MANIFEST_URL = os.getenv("MANIFEST_URL")
-DB_URL = os.getenv("DB_URL")
-DB_ECHO = True
 MNEMONICS = os.getenv("MNEMONICS")
 TONAPI_KEY = os.getenv("TONAPI_KEY")
 TONCONNECT_BRIDGEAPI_KEY = os.getenv("TONCONNECT_BRIDGEAPI_KEY")
-BASE_DIR = Path(__file__).resolve().parent
+
+DB_URL = os.getenv("DB_URL")
+DB_ECHO = True
--- bot/filters/is_admin.py
+++ bot/filters/is_admin.py
@@ -1,7 +1,9 @@
 from aiogram.filters import Filter
 from aiogram import types
 
+from config import ADMINS
+
 
 class IsAdmin(Filter):
     async def __call__(self, message: types.Message) -> bool:
-        return message.from_user.id in []
+        return message.from_user.id in ADMINS
