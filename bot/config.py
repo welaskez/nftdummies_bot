e7b1b74166054cb647688781d72e49329fc2906a
welaskez <nazar.nikitenko.03@mail.ru>
2024-07-22 01:25:51 +0500

    add base config for stickerpack

--- bot/config.py
+++ bot/config.py
@@ -11,10 +11,14 @@ BASE_DIR = Path(__file__).resolve().parent
 ADMINS = os.getenv("ADMINS").split(" ")
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-MANIFEST_URL = os.getenv("MANIFEST_URL")
-MNEMONICS = os.getenv("MNEMONICS")
+BOT_NAME = os.getenv("BOT_NAME")
 TONAPI_KEY = os.getenv("TONAPI_KEY")
-TONCONNECT_BRIDGEAPI_KEY = os.getenv("TONCONNECT_BRIDGEAPI_KEY")
 
 DB_URL = os.getenv("DB_URL")
 DB_ECHO = True
+
+BASE_STICKER_TEMPLATE_PATH = f"{BASE_DIR}/stickers/base_sticker_template.png"
+BASE_FONT_PATH = f"{BASE_DIR}/fonts/Gagalin-Regular.ttf"
+BASE_STICKERS_DIR = f"{BASE_DIR}/stickers"
+STICKER_SET_NAME = f"nftdummies_stickers_by_{BOT_NAME}"
+STICKER_SET_TITLE = "NFT DUMMIES STICKERS"
