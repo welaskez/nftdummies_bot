f7d9781341fe3f352cb914782418f86eb20cc29b
welaskez <nazar.nikitenko.03@mail.ru>
2024-07-22 01:42:45 +0500

    update start handler

--- bot/handlers/user/start.py
+++ bot/handlers/user/start.py
@@ -1,9 +1,13 @@
 from aiogram import Router, types
 from aiogram.filters import Command
 
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from db.crud import create_user
+
 router = Router()
 
 
 @router.message(Command("start"))
-async def newsletter_cmd(message: types.Message):
-    await message.answer("this is start command")
+async def newsletter_cmd(message: types.Message, session: AsyncSession):
+    await create_user(session=session, tg_id=message.from_user.id)
