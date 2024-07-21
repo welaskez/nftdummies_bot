cb570e8a7bfae58a4660c7f320f4e075c74a99d3
welaskez <nazar.nikitenko.03@mail.ru>
2024-07-22 01:58:49 +0500

    fix admin ids

--- bot/config.py
+++ bot/config.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent
 
-ADMINS = os.getenv("ADMINS").split(" ")
+ADMINS = list(map(lambda admin_id: int(admin_id), os.getenv("ADMINS").split(" ")))
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 BOT_NAME = os.getenv("BOT_NAME")
