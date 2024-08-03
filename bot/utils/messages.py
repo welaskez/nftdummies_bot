e572632ae0054c0dd34a12d4c5178bf15825c9b6
welaskez <nazar.nikitenko.03@mail.ru>
2024-08-03 15:58:00 +0500

    add msgs

---
+++ bot/utils/messages.py
@@ -0,0 +1,18 @@
+start = """
+Welcome, {name}
+
+Stay up to date with TON and Jettons prices.
+
+Easily interact with the bot in any chat.
+
+Designed and developed by @nftdummies
+
+Support the bot by donating to <code>UQAc57RDVLGK1A-IiHKUK5NDXri3uL8dF3wJTYDpvl8ljymd</code>"""
+
+admin = """
+Admin commands
+
+/update_stickers - update all stickers (DONT SPAM!!!)
+/add_sticker - add a new sticker
+/delete_sticker - delete an existing sticker
+"""
